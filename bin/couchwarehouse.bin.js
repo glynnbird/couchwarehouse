c590799d0361e6d3d163807a37997e850b93b150
Glynn Bird <glynn.bird@gmail.com>
2018-10-06 09:53:13 +0100

    simplify cli options

--- bin/couchwarehouse.bin.js
+++ bin/couchwarehouse.bin.js
@@ -4,7 +4,7 @@
 const url = process.env.COUCH_URL || 'http://localhost:5984'
 const db = process.env.COUCH_DATABASE
 const args = require('yargs')
-  .option('url', { alias: 'u', describe: 'CouchDB URL', demandOption: !url, default: url })
+  .option('url', { alias: 'u', describe: 'CouchDB URL', default: url })
   .option('database', { alias: 'db', describe: 'CouchDB database name', demandOption: !db, default: db })
   .option('since', { alias: 's', describe: 'Last known CouchDB changes feed token', demandOption: false, default: '0' })
   .help('help')
